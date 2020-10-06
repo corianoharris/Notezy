e97de20ce792612ffed4be9a64a4409a3b134cbe
corianoharris <corianoharris@gmail.com>
2020-10-06 13:26:08 -0500

    📦 NEW: Add _redirects file

--- functions/helpers/airtable.js
+++ functions/helpers/airtable.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
 var Airtable = require('airtable');
 
-const { secret, baseId, db_name } = process.env;
-var base = new Airtable({ apiKey: secret }).base(baseId);
-const table = base(db_name);
+// deploy env
+// const { secret, baseId, db_name } = process.env;
+// var base = new Airtable({ apiKey: secret }).base(baseId);
+// const table = base(db_name);
+
+// local env
+var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+	process.env.AIRTABLE_BASE_ID
+);
+const table = base(process.env.AIRTABLE_TABLE_NAME);
 
 console.log(table);
 
