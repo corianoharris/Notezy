3bf102b6ba3964d803ade1e4a8577e75998a5576
corianoharris <corianoharris@gmail.com>
2020-10-06 13:27:05 -0500

    📦 NEW: reconfig airtable.js for netlify

--- functions/helpers/airtable.js
+++ functions/helpers/airtable.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 var Airtable = require('airtable');
 
 // deploy env
-// const { secret, baseId, db_name } = process.env;
-// var base = new Airtable({ apiKey: secret }).base(baseId);
-// const table = base(db_name);
+const { secret, baseId, db_name } = process.env;
+var base = new Airtable({ apiKey: secret }).base(baseId);
+const table = base(db_name);
 
 // local env
-var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-	process.env.AIRTABLE_BASE_ID
-);
-const table = base(process.env.AIRTABLE_TABLE_NAME);
+// var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+// 	process.env.AIRTABLE_BASE_ID
+// );
+// const table = base(process.env.AIRTABLE_TABLE_NAME);
 
 console.log(table);
 
