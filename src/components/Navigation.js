c453ed08340403c38c6723231418445588ead7ec
Coriano Harris <corianoharris@gmail.com>
2020-10-07 13:13:23 -0500

    📦 NEW:  added app title and note route

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import LogoutButton from './LogoutButton';
 import '../index.css';
 
 const Navigation = () => {
 	return (
 		<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+			<span role="img" aria-label="note emoji">
+				&#128221;
+			</span>
+			<a href={'/'} className="navbar-brand">
+				Note Collector
+			</a>
 			<div className="collpase navbar-collapse">
 				<ul className="navbar-nav mr-auto">
 					<li className="navbar-item">
-						<a href={'/'} className="nav-link">
+						<a href={'/note'} className="nav-link">
 							Note
 						</a>
 					</li>
