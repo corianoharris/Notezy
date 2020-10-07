34fc13782147257258738cc628b3d5a0076fa75e
Coriano Harris <corianoharris@gmail.com>
2020-10-07 15:33:29 -0500

    🎨 STLYE:  added bootstrap classes

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -32,47 +32,51 @@ const CreateNote = () => {
 		<Fragment>
 			<Navigation />
 			<Profile />
-			<div className="container w-100">
-				<div className="text-white">
-					<form className="mt-5 was-validated" onSubmit={submitNote}>
-						<label className="h3 text-warning" htmlFor="note-title">
-							Title
-						</label>
-						<input
-							type="text"
-							className="form-control p-3 bg-white text-warning first-letter"
-							id="note-title"
-							placeholder="add title..."
-							name="note"
-							value={title}
-							onChange={(e) => setTitle(e.target.value)}
-							required
-						/>
-						<div className="valid-feedback mt-2 mb-2">Thank you.</div>
-						<div className="invalid-feedback mt-2 mb-2">
-							Please add a title...
-						</div>
-						<label className="h3 text-warning" htmlFor="note-content">
-							Note
-						</label>
-						<textarea
-							type="text"
-							className="form-control p-3 bg-white text-warning first-letter"
-							id="note-content"
-							placeholder="add note..."
-							name="note-content"
-							value={content}
-							onChange={(e) => setContent(e.target.value)}
-							required></textarea>
-						<div className="valid-feedback mt-2 mb-2">Thank you.</div>
-						<div className="invalid-feedback mt-2 mb-2">
-							Please write a note...
-						</div>
-						<div className="text-right">
-							<button className="btn btn-outline-primary">Add</button>
-						</div>
-					</form>
-				</div>
+			<div
+				className="text-white"
+				className="container w-100"
+				style={{
+					backgroundColor: '#343b3f',
+					padding: '8px',
+				}}>
+				<form className="mt-5 was-validated" onSubmit={submitNote}>
+					<label className="h3 text-warning" htmlFor="note-title">
+						Title
+					</label>
+					<input
+						type="text"
+						className="form-control p-3 bg-white text-warning first-letter"
+						id="note-title"
+						placeholder="add title..."
+						name="note"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						required
+					/>
+					<div className="valid-feedback mt-2 mb-2">Thank you.</div>
+					<div className="invalid-feedback mt-2 mb-2">
+						Please add a title...
+					</div>
+					<label className="h3 text-warning" htmlFor="note-content">
+						Note
+					</label>
+					<textarea
+						type="text"
+						className="form-control p-3 bg-white text-warning first-letter"
+						id="note-content"
+						placeholder="add note..."
+						name="note-content"
+						value={content}
+						onChange={(e) => setContent(e.target.value)}
+						required></textarea>
+					<div className="valid-feedback mt-2 mb-2">Thank you.</div>
+					<div className="invalid-feedback mt-2 mb-2">
+						Please write a note...
+					</div>
+					<div className="text-right m-4">
+						<button className="btn btn-outline-primary">Add</button>
+					</div>
+				</form>
 			</div>
 		</Fragment>
 	);
