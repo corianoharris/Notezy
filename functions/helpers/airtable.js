3ef9928a3260ac8d46f20d8da2054becf44bb990
Coriano Harris <corianoharris@gmail.com>
2020-10-07 10:57:05 -0500

    🏥 REFACTOR: connecting to airtable w/ env vars

--- functions/helpers/airtable.js
+++ functions/helpers/airtable.js
@@ -1,12 +1,6 @@
 require('dotenv').config();
 var Airtable = require('airtable');
 
-// deploy env
-// const { secret, baseId, db_name } = process.env;
-// var base = new Airtable({ apiKey: secret }).base(baseId);
-// const table = base(db_name);
-
-// local env
 var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
 	process.env.AIRTABLE_BASE_ID
 );
