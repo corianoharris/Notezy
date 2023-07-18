d0d09e99d26966485a3824ba69ed14309a0b8649
Coriano Harris <corianoharris@gmail.com>
2023-07-18 16:26:16 -0700

    Fixed SkeletonLoader class error

--- src/components/SkeletonLoader/SkeletonLoader.js
+++ src/components/SkeletonLoader/SkeletonLoader.js
@@ -12,7 +12,7 @@ const SkeletonLoader = () => {
 			{/* < Navigation /> */}
 			<div className='skeleton-loader'>
 				<div className=' skeleton skeleton-loader-header'></div>
-				<div class="skeleton-loader-content-container">
+				<div className="skeleton-loader-content-container">
 					<div className='skeleton skeleton-text'>
 					</div>
 					<div className='skeleton skeleton-text'>
