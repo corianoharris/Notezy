4d2ad0aac59cefcf53dbe617f15be9e565484156
Coriano Harris <corianoharris@gmail.com>
2023-07-24 11:56:44 -0700

    Changed status text to loading

--- src/components/ProtectedPageLoader/ProtectedPageLoader.js
+++ src/components/ProtectedPageLoader/ProtectedPageLoader.js
@@ -6,7 +6,7 @@ const ProtectedPageLoader = () => {
 		<>
 			<div className='text-center p-5 mx-auto border-0 login'>
 				<h2 className="container w-100 mt-5 mx-auto text-center ">
-					Getting your page...
+					Loading...
 				</h2>
 			</div>
 			<Footer />
